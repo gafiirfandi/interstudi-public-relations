a71086ca5b53c73ca99334bee671d1fed828d4ca
Muhammad Destara Syanandi <muhammad.destara@ui.ac.id>
2021-10-31 14:51:23 +0700

    push new project public relations interstudi

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from .views import index
+from .views import index,programlainnya,pendaftaranonline,hubungikami,gallery,biayastudi
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('programlainnya', programlainnya, name="programlainnya"),
+    path('pendaftaranonline', pendaftaranonline, name="pendaftaranonline"),
+    path('hubungikami', hubungikami, name="hubungikami"),
+    path('gallery', gallery, name="gallery"),
+    path('biayastudi', biayastudi, name="biayastudi"),
 ]
